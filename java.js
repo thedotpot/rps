2bab958d8d5096908e66a3bbe091525c808ec47f
Christopher Lee <cleeit117@gmail.com>
2022-06-18 18:10:25 -0400

    More organized. Top comment section is original code

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-function computerPlay(){
+/*function computerPlay(){
     let randomArray = ['Rock', 'Paper', 'Scissors'];
     return randomArray[Math.floor(Math.random()*randomArray.length)];
 }
@@ -26,7 +26,7 @@ const tie = "It's a tie!";
 
 
  
-function game(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){
     if (playerSelection === r && computerSelection === s){
         return win;
     } else if (playerSelection === r && computerSelection === p){
@@ -53,11 +53,65 @@ let computerInput = "Computer has entered " + computerSelection;
 
 console.log(userInput);
 console.log(computerInput);
-console.log(game(playerSelection, computerSelection));
+
+function game(){
+    for (let i = 0; i< 5; i++){
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection))
+    }
+}
+
+console.log(game());
+*/
+
+const playerScore = 0;
+const compScore = 0;
+
+function computerPlay(){
+    const array = ['rocks', 'paper', 'scissors'];
+    return array[Math.floor(Math.random()* array.length)];
+}
+
+const playerSelection = prompt("Choose rock, paper or scissors");
+const computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === 'rock' && computerSelection === 'rock'){
+        return "This was a tie. Rock and Rock ";
+        
+    }else if (playerSelection === 'rock' && computerSelection === 'paper'){
+        return "You have lost. Paper beats rock";
+    }else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+        return "You have won. Rock beats scissors";
+    }else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+        return "You have lost. Scissors beats paper";
+    }else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        return "You have won. Paper beats rock!";
+    }else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+        return "You have lost. Rock beat scissors";
+    }else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        return "You have won. Scissors beats paper";
+    }else if (playerSelection === 'paper' && computerSelection === 'paper'){
+        return "This is a tie. Paper is Paper";
+    }else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
+        return "This is a tie. Scissors is scissors";
+    }else {
+        return "Please enter valid input";
+    }    
+     
+}  
 
 
+function loop(){
+    for (let i = 0; i<5; i++){
+        const computerSelection= computerPlay()
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("You: " + playerSelection);
+        console.log("Computer: " + computerSelection);
+    }
+}
 
-
+loop();
 
 
 
