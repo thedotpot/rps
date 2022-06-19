00bc7ef0477a443e76ea69719d86145e531327be
Christopher Lee <cleeit117@gmail.com>
2022-06-19 13:30:20 -0400

    Added prompt to looping function

--- java.js
+++ java.js
@@ -1,12 +1,12 @@
-const playerScore = 0;
-const compScore = 0;
+let playerScore = 0;
+let compScore = 0;
 
 function computerPlay(){
     const array = ['rocks', 'paper', 'scissors'];
     return array[Math.floor(Math.random()* array.length)];
 }
 
-const playerSelection = prompt("Choose rock, paper or scissors");
+
 const computerSelection = computerPlay();
 
 function playRound(playerSelection, computerSelection){
@@ -14,16 +14,22 @@ function playRound(playerSelection, computerSelection){
         return "This was a tie. Rock and Rock ";
         
     }else if (playerSelection === 'rock' && computerSelection === 'paper'){
+        compScore++
         return "You have lost. Paper beats rock";
     }else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+        playerScore++
         return "You have won. Rock beats scissors";
     }else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+        compScore++
         return "You have lost. Scissors beats paper";
     }else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        playerScore++
         return "You have won. Paper beats rock!";
     }else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+        compScore++
         return "You have lost. Rock beat scissors";
     }else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        playerScore++
         return "You have won. Scissors beats paper";
     }else if (playerSelection === 'paper' && computerSelection === 'paper'){
         return "This is a tie. Paper is Paper";
@@ -38,15 +44,35 @@ function playRound(playerSelection, computerSelection){
 
 function loop(){
     for (let i = 0; i<5; i++){
+        const playerSelection = prompt("Choose rock, paper or scissors", "Rock, Paper, Scissors");
         const computerSelection= computerPlay()
-        console.log(playRound(playerSelection, computerSelection));
+        playRound(playerSelection, computerSelection);
         console.log("You: " + playerSelection);
         console.log("Computer: " + computerSelection);
         
     }
+    if (playerScore > compScore){
+        return 'You have won! Good job!'
+    }else if (compScore> playerScore){
+        return 'You have lost to the computer'
+    }else {
+        return 'You have TIED!'
+    }
 }
 
-loop();
+function yourScore(){
+    return playerScore
+}
+function computerScore(){
+    return compScore
+}
+
+console.log(loop());
+console.log('Your Score:' + yourScore());
+console.log('Computer Score:' + computerScore());
+
+
+
 
 
 
